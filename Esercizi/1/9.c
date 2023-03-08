4b0c5cce89e8c88e4534a941c6b5f090bc5b9c91
Polliog <polliogiuseppe@live.it>
2023-03-08 18:57:48 +0100

    .ignore added

--- Esercizi/1/9.c
+++ Esercizi/1/9.c
@@ -20,7 +20,7 @@ int main() {
 
     x1 = (-b + sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
     x2 = (-b - sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
-    
+
 
     printf("x1: %lf\n", x1);
     printf("x2: %lf\n", x2);
